01cde9b5f8fd709e9ac9bb0c85a609eeea09e6fb
Ahmet Goenan <ahmet.goenan@sap.com>
2016-05-18 15:31:19 +0200

    broadcasting

--- alt_ionicTab/www/js/controllers/beaconControllers.js
+++ alt_ionicTab/www/js/controllers/beaconControllers.js
@@ -2,13 +2,7 @@ angular.module('BeaconApp.beaconControllers', [])
 
 //BeaconController
 .controller("BeaconCtrl", function($scope, $state, $rootScope, $ionicPlatform, $cordovaBeacon, $ionicPopup) {
-      $scope.broadcast = function(id) {
-         
-           if(id == 12){
-             $state.go('tab.information');
-        }
-         
-     }
+     
  
     $scope.beacons = {};
 
@@ -63,6 +57,91 @@ angular.module('BeaconApp.beaconControllers', [])
         $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("Softwareengineering", "2C96200F-07B9-422F-89B1-D7EC8EEB7CD7"));
         
 });
+    
+    
+    
+    
+    
+     $scope.broadcast = function(beacon) {
+         
+      
+         
+           if(beacon.minor == 11 && (beacon.proximity == 'ProximityImmediate' || beacon.proximity == 'ProximityNear')){          
+               
+var strVar="";
+strVar += " <div class=\"my-header\">";
+strVar += "			<img class=\"my-header-image\" src=\"img\/eam.jpg\" \/>";
+strVar += "			<img class=\"my-icon-back\" src=\"img\/icon-back.png\" ontouchend=\"history.back()\" \/>";
+strVar += "		<\/div>";
+strVar += "        ";
+strVar += "		<div class=\"my-content\">";
+strVar += "			<h1>Seminar Schedule<\/h1>";
+strVar += "			<p>Prof. Dr. Colin Atkinson<\/p>";
+strVar += " ";
+strVar += "        <ul class=\"list\">          ";
+strVar += "            <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "             <li class=\"item\">";
+strVar += "               09:45 ahha";
+strVar += "            <\/li>";
+strVar += "<\/ul>";
+
+$('#beacon').append("");
+               
+               var element = $(strVar);
+		$('#beacon').append(element);
+               
+               
+               
+               
+               
+                    $state.go('tab.information');
+            }
+         
+     }
     
      });
 
--- alt_ionicTab/www/js/controllers/informationControllers.js
+++ alt_ionicTab/www/js/controllers/informationControllers.js
@@ -4,7 +4,7 @@ angular.module('BeaconApp.informationControllers', [])
 .controller("InformationCtrl", function($scope, $rootScope, $ionicPlatform) {
 
     
-    
+  
        
    //   var pageId = pluginResult.region.identifier
 
